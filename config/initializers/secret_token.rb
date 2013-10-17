a9f5cc3d2b2e723688b5864419297954376e0c07
Laurie Young <laurie@wildfalcon.com>
2013-10-17 11:30:00 +0100

    Initial import of empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PersonalWire::Application.config.secret_key_base = '9983ddc0497ff75e38d69e3193e134ec51d70db595209ab65ab9455f128a6f6d81ab7609f3d29579614a4454c72626a2fa423edfc70bbbef7ef7eaaf6b6c3f78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalWire::Application.config.session_store :cookie_store, key: '_personal-wire_session'
